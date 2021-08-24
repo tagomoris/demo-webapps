e3c0eda47d7c6705490ddf85a4ed355efd5640d3
Satoshi Moris Tagomori <tagomoris@gmail.com>
2021-08-24 11:21:48 +0900

    use middleware as less as possible

--- rails/demo_app/config/application.rb
+++ rails/demo_app/config/application.rb
@@ -18,5 +18,30 @@ module DemoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.delete Rack::MiniProfiler
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete ActiveSupport::Cache::Strategy::LocalCache::Middleware
+    config.middleware.delete Rack::MethodOverride
+    config.middleware.delete ActionDispatch::RequestId
+    config.middleware.delete ActionDispatch::RemoteIp
+    config.middleware.delete Sprockets::Rails::QuietAssets
+    config.middleware.delete Rails::Rack::Logger
+    config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete WebConsole::Middleware
+    config.middleware.delete ActionDispatch::DebugExceptions
+    config.middleware.delete ActionDispatch::ActionableExceptions
+    config.middleware.delete ActionDispatch::Reloader
+    config.middleware.delete ActionDispatch::Callbacks
+    config.middleware.delete ActiveRecord::Migration::CheckPending
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
+    config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
+    config.middleware.delete ActionDispatch::PermissionsPolicy::Middleware
+    config.middleware.delete Rack::Head
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ETag
+    config.middleware.delete Rack::TempfileReaper
   end
 end
